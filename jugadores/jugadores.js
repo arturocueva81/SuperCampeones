7a5d2f9d25221b241615dc86b95c05141c6444d7
arturocueva81 <ajcs81@hotmail.com>
2026-06-17 22:01:04 -0500

    agrega funcion constructora crearJugador

--- jugadores/jugadores.js
+++ jugadores/jugadores.js
@@ -1,2 +1,20 @@
 //arreglo global vacio
 const CROMOSMUNDIAL = [];
+
+//funcion constructora
+function crearJugador(id, nombre, pais, posicion, urlImagen, urlBandera, colorFondoHex, goles, partidos, destacado) {
+    return {
+        id,
+        nombre,
+        pais,
+        posicion,
+        urlImagen,
+        urlBandera,
+        colorFondoHex,
+        estadisticas: {
+            goles,
+            partidos
+        },
+        destacado: Boolean(destacado)
+    };
+}
